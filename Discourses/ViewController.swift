1c1f496ad829978fedc844838e9acb96b796b5dc
AbhishekMarda <abhishek1marda@ucla.edu>
2020-07-09 16:13:17 +0530

    Trial 2

--- Discourses/ViewController.swift
+++ Discourses/ViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    let myFunTimes : String = "hello world"
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view.
     }
     //trial commit
