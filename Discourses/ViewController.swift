242de6e4b6b7f71a1bc951225ca364295a632b7b
AbhishekMarda <abhishek1marda@ucla.edu>
2020-07-09 16:00:23 +0530

    Initial Commit

---
+++ Discourses/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Discourses
+//
+//  Created by Abhishek Marda on 7/9/20.
+//  Copyright © 2020 DiscoursesTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
