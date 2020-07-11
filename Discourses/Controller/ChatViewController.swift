c05df2b508d03720c7a1bf9704f289109c620509
AbhishekMarda <abhishek1marda@ucla.edu>
2020-07-11 14:02:44 +0530

    More additions to image sets, added elements to ChatView

--- Discourses/Controller/ChatViewController.swift
+++ Discourses/Controller/ChatViewController.swift
@@ -14,7 +14,10 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var chatTable: UITableView!
     
     @IBOutlet weak var backButton: UIButton!
-    let myFunTimes : String = "hello world"
+    
+    @IBOutlet weak var inputField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,11 +25,27 @@ class ChatViewController: UIViewController {
         subjectLabel.font = UIFont(name: "Acme-Regular", size: 30)
         subjectLabel.text = subjectLabel.text?.uppercased()
         
+        
         chatTable.layer.cornerRadius = 40
+        inputField.layer.masksToBounds = true
+        inputField.layer.cornerRadius = 15
+        
+//        let attachmentButton = UIButton(type: .custom)
+//        attachmentButton.setImage(UIImage(systemName: "paperclip"), for: .normal)
+//        attachmentButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: -16, bottom: 0, right: 0)
+//        attachmentButton.frame = CGRect(x: CGFloat(txt.frame.size.width - 25), y: CGFloat(5), width: CGFloat(25), height: CGFloat(25))
+//        attachmentButton.addTarget(self, action: #selector(self.attachmentButtonPressed(_:)), for: .touchUpInside)
+//        inputField.rightView = attachmentButton
+//        inputField.rightViewMode = .always
     }
+    
+    @IBAction func attachmentButtonPressed(_ sender: UIButton){
+        
+    }
+    
     @IBAction func backButtonPressed(_ sender: UIButton) {
         self.dismiss(animated: true) {
-            print("going back")
+            print("Dismissing current view controller")
         }
     }
     
